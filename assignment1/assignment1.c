6292614770bb4c4cd36813a346f389c81ab833dd
Derek Peterson <derekepterson.dpp@gmail.com>
2017-02-22 17:10:09 -0500

    Intial commit

---
+++ assignment1/assignment1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+
+    // Read in integer from command line
+    int N;
+
+    if (argc != 2) {
+        printf("Please enter an integer\n");
+        exit(-1);
+    }
+    N = atoi(argv[1]);
+
+    // Read in delimited file
+
+    // Allocate memory for data
+
+    // Execute the computation
+
+    // Free the memory
+}
